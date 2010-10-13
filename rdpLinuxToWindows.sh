111b56c3cf1f56ff720764228b4fcf7b8f71519b
gsvolt <gauravdotsaxena@gmail.com>
2010-10-13 14:29:42 -0400

    first commit, script for remote desktop

---
+++ rdpLinuxToWindows.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# rdesktop
+
+# in linux if you frequently need to access
+# other windows machines, check out rdesktop
+# it's mighty handy
+
+# author: @gsvolt
+# last updated: 10/12/2010
+# See Also: info or man rdesktop
+
+rdesktop -u me -p mypassword -g 800x600 ipaddress:port &
